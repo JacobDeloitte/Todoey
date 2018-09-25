827b8d9ae08f18fd19d27c4f2f8f063c9f52a1d0
Bartlett, Jacob (UK - London) <jacbartlett@deloitte.co.uk>
2018-09-25 14:16:07 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Bartlett, Jacob (UK - London) on 24/09/2018.
+//  Copyright © 2018 Jacob. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
